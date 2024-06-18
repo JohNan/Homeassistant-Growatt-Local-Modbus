b19249d1667078620c632eb5a5da98d27bb9e477
Johan Nenzén <johan.nanzen@gmail.com>
2024-06-18 23:11:28 +0200

    Update __init__.py

--- custom_components/growatt_local/__init__.py
+++ custom_components/growatt_local/__init__.py
@@ -96,7 +96,7 @@ async def async_setup_entry(
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     async def update_listener(hass, entry):
-    """Handle options update."""
+        """Handle options update."""
         await hass.config_entries.async_reload(entry.entry_id)
     
     entry.async_on_unload(entry.add_update_listener(update_listener))
